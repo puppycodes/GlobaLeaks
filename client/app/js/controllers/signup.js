5cdf19bb7d8d46020f000ae8efc57ab8f8f2e946
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2018-07-04 20:30:15 +0200

    Implement whistleblowing.it signup

--- client/app/js/controllers/signup.js
+++ client/app/js/controllers/signup.js
@@ -1,5 +1,9 @@
-    GLClient.controller('SignupCtrl', ['$scope', '$location', '$route', '$http', 'Authentication', 'CONSTANTS',
-                    function($scope, $location, $route, $http, Authentication, CONSTANTS) {
+    GLClient.controller('SignupCtrl', ['$scope', '$location', '$route', '$http', 'Authentication', 'CONSTANTS', 'DATA_COUNTRIES_ITALY_REGIONS', 'DATA_COUNTRIES_ITALY_PROVINCES', 'DATA_COUNTRIES_ITALY_CITIES',
+                    function($scope, $location, $route, $http, Authentication, CONSTANTS, DATA_COUNTRIES_ITALY_REGIONS, DATA_COUNTRIES_ITALY_PROVINCES, DATA_COUNTRIES_ITALY_CITIES) {
+  $scope.data_countries_italy_regions = DATA_COUNTRIES_ITALY_REGIONS.query();
+  $scope.data_countries_italy_provinces = DATA_COUNTRIES_ITALY_PROVINCES.query();
+  $scope.data_countries_italy_cities = DATA_COUNTRIES_ITALY_CITIES.query();
+
   $scope.email_regexp = CONSTANTS.email_regexp;
 
   $scope.hostname = $location.search().hostname || '';
@@ -9,9 +13,24 @@
     'subdomain': '',
     'name': '',
     'surname': '',
+    'role': '',
+    'phone': '',
     'email': '',
+    'secondary_email': '',
     'use_case': '',
-    'use_case_other': ''
+    'use_case_other': '',
+    'organization_name': '',
+    'organization_type': '',
+    'organization_location1': '',
+    'organization_location2': '',
+    'organization_location3': '',
+    'organization_location4': '',
+    'organization_number_employees': '',
+    'organization_number_users': '',
+    'hear_channel': '',
+    'hear_channel_other': '',
+    'tos1': false,
+    'tos2': false
   };
 
   var completed = false;
